9fa1c91404303e1062bc6f2667949b36b2608ed2
Matthew Ogtong <ogtongm@gmail.com>
2023-02-07 18:33:33 -0500

    Initial Commit

---
+++ SynthMind/SynthMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynthMindApp.swift
+//  SynthMind
+//
+//  Created by Matthew Ogtong on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SynthMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
