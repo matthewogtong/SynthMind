0e81bc7c1cae03f1db13375a6d85cf92eebead11
Matthew Ogtong <ogtongm@gmail.com>
2023-02-08 12:48:54 -0500

    Add OpenAI response models for completions API

--- SynthMind/Services/OpenAIService.swift
+++ SynthMind/Services/OpenAIService.swift
@@ -16,15 +16,25 @@ class OpenAIService {
         let headers: HTTPHeaders = [
             "Authorization": "Bearer \(String(describing: Constants.openAIAPIKey!))"
         ]
-        AF.request(baseUrl + "completions", method: .post, parameters: body, encoder: .json, headers: headers).responseString { data in
-            print(data.result)
+        AF.request(baseUrl + "completions", method: .post, parameters: body, encoder: .json, headers: headers).responseDecodable(of: OpenAICompletionsResponse.self) { response in
+            print(response.result)
         }
     }
 }
+
 
 struct OpenAICompletionsBody: Encodable {
     let model: String
     let prompt: String
     let temperature: Float?
+}
+
+struct OpenAICompletionsResponse: Decodable {
+    let id: String
+    let choices: [OpenAICompletionsChoices]
+}
+
+struct OpenAICompletionsChoices: Decodable {
+    let text: String
 }
 
