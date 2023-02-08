ad6519d1e71492b9590cc203624cce308a546f52
Matthew Ogtong <ogtongm@gmail.com>
2023-02-08 12:02:24 -0500

    Add OpenAIService class for sending API requests using Alamofire and defining body structure for OpenAI completions API

---
+++ SynthMind/Services/OpenAIService.swift
@@ -0,0 +1,31 @@
+//
+//  OpenAIService.swift
+//  SynthMind
+//
+//  Created by Matthew Ogtong on 2/8/23.
+//
+
+import Foundation
+import Alamofire
+
+class OpenAIService {
+    let baseUrl = "https://api.openai.com/v1/"
+    
+    func sendMessage(message: String) {
+        let body = OpenAICompletionsBody(model: "text-davinci-003", prompt: message, temperature: 0.7)
+        let headers: HTTPHeaders = [
+            "Authorization": "Bearer \(String(describing: Constants.openAIAPIKey))"
+        ]
+        
+        AF.request(baseUrl + "completions", method: .post, parameters: body, encoder: .json, headers: headers).response { data in
+            print(data)
+        }
+    }
+}
+
+struct OpenAICompletionsBody: Encodable {
+    let model: String
+    let prompt: String
+    let temperature: Float?
+}
+
